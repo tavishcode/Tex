2bafef1e9b4cfbdc199a3c5f5a466307acef2372
tavishcode <tavish.gobindram@gmail.com>
2016-10-02 14:22:56 +0800

    first commit

---
+++ app/src/main/java/com/tex/texandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tex.texandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
